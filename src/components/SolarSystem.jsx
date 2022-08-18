926f3448ad294a4f448a77725b2943bcccb3894b
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-18 16:36:39 -0300

    requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((e) => (
+          <PlanetCard key={ e.name } planetName={ e.name } planetImage={ e.image } />))}
       </div>
     );
   }
