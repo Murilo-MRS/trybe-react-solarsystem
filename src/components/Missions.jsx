f28331fb95fd109281773cd4be5f727844cc0fc1
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-18 17:12:49 -0300

    requisito 9

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import MissionsCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((e) => (
+          <MissionsCard
+            key={ e.name }
+            name={ e.name }
+            year={ e.year }
+            country={ e.country }
+            destination={ e.destination }
+          />))}
       </div>
     );
   }
