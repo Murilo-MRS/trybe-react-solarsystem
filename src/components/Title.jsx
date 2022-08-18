9c11c83fec895853e2e3c6ddf61914ad72b60ccf
Murilo-MRS <murilosantana1993@gmail.com>
2022-08-18 15:54:12 -0300

    requisito 3 e 4 corrigido

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
@@ -8,5 +9,9 @@ class Title extends React.Component {
     );
   }
 }
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Title;
